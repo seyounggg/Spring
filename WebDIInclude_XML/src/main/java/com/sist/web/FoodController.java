36a899c7a86958c1cfa86d3d7c537a6c1a1ba1e8
seyounggg <syn_95@naver.com>
2023-08-01 17:42:43 +0900

    Cookie 추가(home - 최근 방문 목록 추가)

--- WebDIInclude_XML/src/main/java/com/sist/web/FoodController.java
+++ WebDIInclude_XML/src/main/java/com/sist/web/FoodController.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import com.sist.dao.*;
 import com.sist.vo.*;
 
@@ -25,6 +30,26 @@ public class FoodController {
 		model.addAttribute("main_jsp", "../food/food_list.jsp");
 		return "main/main";
 	}
+	
+	@GetMapping("food/food_before_detail.do")
+	public String food_before_detail(int fno,RedirectAttributes ra,
+										HttpServletResponse response) {
+		/*
+		 	Model : forward일때 값 전송
+		 	RedirectAttribute : sendRedirect일때 값 전송
+		 */
+		Cookie cookie=new Cookie("food_"+fno, String.valueOf(fno));
+		// cookie의 매개변수가(String, String)이라서 int인 fno를 그대로 사용하면 오류 발생함.
+		// cookie는 String으로만 저장이 가능하다(데이터값)
+		cookie.setPath("/");
+		cookie.setMaxAge(60*60*24);
+		// 브라우저로 전송
+		response.addCookie(cookie);
+		ra.addAttribute("fno", fno); // 값을 출력하면서 이동
+		//ra.addFlashAttribute(attributeValue); - 값을 감추고 이동
+		return "redirect:../food/food_detail.do";
+	}
+
 	
 	@GetMapping("food/food_detail.do")
 	public String food_detail(int fno,Model model) {
--- WebDIInclude_XML/src/main/java/com/sist/web/MainController.java
+++ WebDIInclude_XML/src/main/java/com/sist/web/MainController.java
@@ -6,6 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import com.sist.dao.*;
 import com.sist.vo.*;
 
@@ -17,14 +21,30 @@ public class MainController {
 	
 	//사용자 요청 처리
 	@GetMapping("main/main.do")
-	public String main_main(String cno,Model model) {
-		if(cno==null)
-			cno="1";
-		Map map=new HashMap();
-		map.put("cno", Integer.parseInt(cno));
-		List<CategoryVO> list=dao.foodCategoryData(map);
-		model.addAttribute("list", list);
-		model.addAttribute("main_jsp", "../main/home.jsp");
-		return "main/main";
+	public String main_main(String cno,Model model,HttpServletRequest request) {
+		   if(cno==null)
+			   cno="1";
+		   Map map=new HashMap();
+		   map.put("cno", Integer.parseInt(cno));
+		   List<CategoryVO> list=dao.foodCategoryData(map);
+		   model.addAttribute("list", list);
+		   
+		   Cookie[] cookies=request.getCookies();
+		   List<FoodVO> cList=new ArrayList<FoodVO>();
+		   if(cookies!=null)
+		   {
+			   for(int i=cookies.length-1;i>=0;i--)
+			   {
+				   if(cookies[i].getName().startsWith("food_"))
+				   {
+					   String no=cookies[i].getValue();
+					   FoodVO vo=dao.foodDetailData(Integer.parseInt(no));
+					   cList.add(vo);
+				   }
+			   }
+		   }
+		   model.addAttribute("cList", cList);
+		   model.addAttribute("main_jsp", "../main/home.jsp");
+		   return "main/main";
 	}
 }
